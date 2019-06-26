b80a3a888da1d69acbfee7d737274654b432a0a9
Casey <caseygierke@gmail.com>
2019-06-25 21:24:54 -0600

    Require login to post, remove or approve comments.

--- blog/views.py
+++ blog/views.py
@@ -84,10 +84,12 @@ def add_comment_to_post(request, pk):
 		form = CommentForm()
 	return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
+@login_required
 def comment_remove(request, pk):
 	comment = get_object_or_404(Comment, pk=pk)
 	comment.delete()
 
+@login_required
 def comment_approve(request, pk):
 	# mydjangosite.com/comment/2/approve --> comment 2 will get approved
 	comment = get_object_or_404(Comment, pk=pk)
